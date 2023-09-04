70868c23b6329b323ebb8c09da61e8c1aaf43609
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-09-04 21:39:44 +0300

    add

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,22 +1,49 @@
 import { NavLink } from 'react-router-dom';
+import './Navigation.css';
+
+// export default function Navigation() {
+//     return (
+//         <nav className='navbar'>
+//             <NavLink
+//                 exact to="/"
+//                 className='nav-link'
+//                 activeClassname='active-link'>Головна</NavLink>
+            
+//             <NavLink
+//                 to="/authors"
+//                 className='nav-link'
+//                 activeClassname='active-link'>Автори</NavLink>
+            
+//             <NavLink
+//                 to="/boocks"
+//                 className='nav-link'
+//                 activeClassname='active-link'>Книги</NavLink>
+//         </nav>
+//     )
+// }
+
+
 
 export default function Navigation() {
     return (
-        <nav>
+        <nav className="navbar">
             <NavLink
-                exact to="/"
-                className=''
-                activeClassname=''>Головна</NavLink>
+                to="/faq"
+                style={isActive => ({
+                color: isActive ? "green" : "blue"
+                })}>
+                FAQs
+            </NavLink>
             
             <NavLink
                 to="/authors"
-                className=''
-                activeClassname=''>Автори</NavLink>
+                className="nav-link"
+                activeClassName="active-link">Автори</NavLink>
             
             <NavLink
-                to="/boocks"
-                className=''
-                activeClassname=''>Книги</NavLink>
+                to="/books"
+                className="nav-link"
+                activeClassName="active-link">Книги</NavLink>
         </nav>
-    )
+    );
 }
